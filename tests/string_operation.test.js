a33702ea3a5e79432d57c69e1316abac12a678e3
simoxsimo <touili.mohammed@gmail.com>
2020-06-16 15:07:01 +0000

    initial commit

--- tests/string_operation.test.js
+++
@@ -1,9 +0,0 @@
-import stringOp from '../scripts/string_operation';
-
-test('capitalize the word food', () => {
-  expect(stringOp.capitalize('food')).toBe('Food');
-});
-
-test('reverse the word program', () => {
-  expect(stringOp.reverseString('program')).toBe('margorp');
-});
