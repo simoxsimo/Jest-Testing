bde6dcb3b13cc07ad16c01fef9d607c82975816f
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-06-17 15:48:33 +0100

    Implemented analyze method

---
+++ scripts/analysis.js
@@ -0,0 +1,21 @@
+const analyze = (array) => {
+    const obj = {};
+    let sum = 0;
+    let max = array[0];
+    let min = array[0];
+    array.forEach(num => {
+    sum += num;
+    if (max < num) {
+    max = num;
+    }
+    else if (min > num) {
+    min = num;
+    }
+    });
+    obj.average = sum / array.length;
+    obj.min = min;
+    obj.max = max;
+    obj.length = array.length;
+    return obj;
+   }
+   export default analyze;
